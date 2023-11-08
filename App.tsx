d44a73f9d8bff14b1026ef45f5062e086ae8f968
qwertydrf <diego.drf4@outlook.com>
2023-11-07 21:48:34 -0300

    feat(initial): commit

---
+++ App.tsx
@@ -0,0 +1,21 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import SignInScreen from './screens/SignInScreen';
+import HomeScreen from './screens/HomeScreen';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="SignIn">
+        <Stack.Screen name="SignIn" component={SignInScreen} />
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ headerBackVisible: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
